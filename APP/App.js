5b9ea4579427c4a687c8096bee196f6699124932
enjoytheholiday <38365935+enjoytheholiday@users.noreply.github.com>
2018-04-17 17:56:53 -0400

    version 100

--- APP/App.js
+++ APP/App.js
@@ -5,9 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>yes????????????????????????!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>version 100</Text>
       </View>
     );
   }
