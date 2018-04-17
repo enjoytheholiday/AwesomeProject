02f156d6db824dfd57a323d96b9b6be8c8afcd63
moptix10 <38365802+moptix10@users.noreply.github.com>
2018-04-17 19:08:24 -0300

    102

--- APP/App.js
+++ APP/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>version 102</Text>
+        <Text>version 103</Text>
       </View>
     );
   }
